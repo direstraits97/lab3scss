db0bc297bf167b27264b6907393f949cff070f53
joba <josefine.backlund@hotmail.com>
2026-02-04 09:59:21 +0100

    first commit

---
+++ vite.config.js
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        sass: resolve(__dirname, "sass.html"),
+      },
+    },
+  },
+  plugins: [
+    ViteImageOptimizer({
+      png: {
+        quality: 80,
+      },
+      avif: {
+        quality: 70,
+      },
+    }),
+  ],
+});
