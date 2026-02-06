63eab03dc6926fe08587af84140f4b238f579202
joba <josefine.backlund@hotmail.com>
2026-02-06 10:01:35 +0100

    Innehåll på SASS-undersidan samt småfix efter kodvalidering.

--- src/js/main.js
+++ src/js/main.js
@@ -1,16 +1,19 @@
 "use strict";
 
+//Utvalda queryselectors som ska användas.
 document.addEventListener("DOMContentLoaded", () => {
   const nav = document.querySelector("#navbar");
   const openMenu = document.querySelector("#openmenu");
   const closeMenu = document.querySelector("#closemenu");
 
-  if (window.innerWidth < 600) {
+  //Oöppnad navigering kommer ej störa skärmläsare.
+  if (window.innerWidth < 1024) {
     nav.setAttribute("inert", "");
   } else {
     nav.removeAttribute("inert");
   }
 
+  //Eventlyssnare som tar fram och skjuter undan navigering samt lägger till och tar bort aria-labels med värden som underlättar för skärmläsare.
   openMenu.addEventListener("click", () => {
     nav.classList.add("show");
     openMenu.setAttribute("aria-expanded", "true");
